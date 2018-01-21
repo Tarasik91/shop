7becc4f2f12485f320627d8bf6c141d8d940b6a8
Admin MicroGIS <support@micro-gis.com>
2018-01-21 23:10:43 +0200

    add conf

--- src/main/java/com/myshop/controller/s3/S3File.java
+++ src/main/java/com/myshop/controller/s3/S3File.java
@@ -1,55 +1,67 @@
 package com.myshop.controller.s3;
 
-import javax.persistence.Entity;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
 
-@Entity
-public class S3File{// extends Model {
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
-//	@Id
-//	public UUID id;
-//
-//	private String bucket;
-//
-//	public String name;
-//
-//	@Transient
-//	public File file;
-//
-//	public URL getUrl() throws MalformedURLException {
-//		return new URL("https://s3.amazonaws.com/" + bucket + "/" + getActualFileName());
-//	}
-//
-//	private String getActualFileName() {
-//		return id + "/" + name;
-//	}
-//
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.myshop.plugins.S3Plugin;
+
+import play.Logger;
+
+//@Entity
+public class S3File{ //extends Model {
+
+	@Id
+	public UUID id;
+
+	private String bucket;
+
+	public String name;
+
+	@Transient
+	public File file;
+
+	public URL getUrl() throws MalformedURLException {
+		return new URL("https://s3.amazonaws.com/" + bucket + "/" + getActualFileName());
+	}
+
+	private String getActualFileName() {
+		return id + "/" + name;
+	}
+
 //	@Override
-//	public void save() {
-//		if (S3Plugin.amazonS3 == null) {
-//			Logger.error("Could not save because amazonS3 was null");
-//			throw new RuntimeException("Could not save");
-//		} else {
-//			this.bucket = S3Plugin.s3Bucket;
-//
-//			super.save(); // assigns an id
-//
-//			PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, getActualFileName(), file);
-//			putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead); // public
-//																				// for
-//																				// all
-//			S3Plugin.amazonS3.putObject(putObjectRequest); // upload file
-//		}
-//	}
-//
-//	@Override
-//	public void delete() {
-//		if (S3Plugin.amazonS3 == null) {
-//			Logger.error("Could not delete because amazonS3 was null");
-//			throw new RuntimeException("Could not delete");
-//		} else {
-//			S3Plugin.amazonS3.deleteObject(bucket, getActualFileName());
-//			super.delete();
-//		}
-//	}
+	public void save() {
+		if (S3Plugin.amazonS3 == null) {
+			Logger.error("Could not save because amazonS3 was null");
+			throw new RuntimeException("Could not save");
+		} else {
+			this.bucket = S3Plugin.s3Bucket;
+
+	//		super.save(); // assigns an id
+
+			PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, getActualFileName(), file);
+			putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead); // public
+																				// for
+																				// all
+			S3Plugin.amazonS3.putObject(putObjectRequest); // upload file
+		}
+	}
+
+	//@Override
+	public void delete() {
+		if (S3Plugin.amazonS3 == null) {
+			Logger.error("Could not delete because amazonS3 was null");
+			throw new RuntimeException("Could not delete");
+		} else {
+			S3Plugin.amazonS3.deleteObject(bucket, getActualFileName());
+		//	super.delete();
+		}
+	}
 
 }
--- src/main/java/com/myshop/plugins/S3Plugin.java
+++ src/main/java/com/myshop/plugins/S3Plugin.java
@@ -1,18 +1,19 @@
 package com.myshop.plugins;
 
-/*import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import play.Application;
 import play.Logger;
 import play.Plugin;
-*/
-public class S3Plugin{// extends Plugin {
-/*
-    public static final String AWS_S3_BUCKET = "malyatko";
-    public static final String AWS_ACCESS_KEY = "AKIAJJA4GBZ6UKEVVBEQ";
-    public static final String AWS_SECRET_KEY = "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";
+
+public class S3Plugin extends Plugin {
+
+	public static final String AWS_S3_BUCKET = "aws.s3.bucket";
+    public static final String AWS_ACCESS_KEY = "aws.access.key";
+    public static final String AWS_SECRET_KEY = "aws.secret.key";
+
     private final Application application;
 
     public static AmazonS3 amazonS3;
@@ -43,5 +44,5 @@ public class S3Plugin{// extends Plugin {
                 application.configuration().keys().contains(AWS_SECRET_KEY) &&
                 application.configuration().keys().contains(AWS_S3_BUCKET));
     }
-*/
+
 }
